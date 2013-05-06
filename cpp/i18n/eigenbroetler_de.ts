65d3883572e57db27d627eb05b9c7739588de57e
Michael Oldfield <magister_ludi_github@yahoo.com.au>
2013-05-06 17:15:39 +1000

    Updated translation file for pending commit data.

--- cpp/i18n/eigenbroetler_de.ts
+++ cpp/i18n/eigenbroetler_de.ts
@@ -68,165 +68,188 @@
 <context>
     <name>EigenbrotWindow</name>
     <message>
-        <location filename="../source/eigenbrot_window.cpp" line="49"/>
+        <location filename="../source/eigenbrot_window.cpp" line="48"/>
         <source>Ready</source>
         <translation>Fertig</translation>
     </message>
     <message>
-        <location filename="../source/eigenbrot_window.cpp" line="64"/>
+        <location filename="../source/eigenbrot_window.cpp" line="63"/>
         <source>&amp;New...</source>
         <translation>&amp;Neu...</translation>
     </message>
     <message>
-        <location filename="../source/eigenbrot_window.cpp" line="66"/>
+        <location filename="../source/eigenbrot_window.cpp" line="65"/>
         <source>Create a new complex array</source>
         <translation>Einen neuen komplexen Datensatz erzeugen</translation>
     </message>
     <message>
-        <location filename="../source/eigenbrot_window.cpp" line="69"/>
+        <location filename="../source/eigenbrot_window.cpp" line="68"/>
         <source>&amp;Open...</source>
         <translation>&amp;Offnen...</translation>
     </message>
     <message>
-        <location filename="../source/eigenbrot_window.cpp" line="70"/>
+        <location filename="../source/eigenbrot_window.cpp" line="69"/>
         <source>Open an image file</source>
         <translation>Eine Bilddatei öffnen</translation>
     </message>
     <message>
-        <location filename="../source/eigenbrot_window.cpp" line="73"/>
+        <location filename="../source/eigenbrot_window.cpp" line="72"/>
         <source>&amp;Save as...</source>
         <translation>&amp;Speichern unter...</translation>
     </message>
     <message>
-        <location filename="../source/eigenbrot_window.cpp" line="74"/>
+        <location filename="../source/eigenbrot_window.cpp" line="73"/>
         <source>Ctrl+S</source>
         <translation>Ctrl+S</translation>
     </message>
     <message>
-        <location filename="../source/eigenbrot_window.cpp" line="75"/>
+        <location filename="../source/eigenbrot_window.cpp" line="74"/>
         <source>Save complex array to disk</source>
         <translation>Datensatz auf die Festplatte speichern</translation>
     </message>
     <message>
-        <location filename="../source/eigenbrot_window.cpp" line="77"/>
+        <location filename="../source/eigenbrot_window.cpp" line="78"/>
         <source>E&amp;xit</source>
         <translation>Sch&amp;ließen</translation>
     </message>
     <message>
-        <location filename="../source/eigenbrot_window.cpp" line="79"/>
+        <location filename="../source/eigenbrot_window.cpp" line="80"/>
         <source>Close %1</source>
         <translation>%1 schließen</translation>
     </message>
     <message>
-        <location filename="../source/eigenbrot_window.cpp" line="84"/>
+        <location filename="../source/eigenbrot_window.cpp" line="85"/>
         <source>&amp;Real/imaginary</source>
         <translation>&amp;Reell/imaginär</translation>
     </message>
     <message>
-        <location filename="../source/eigenbrot_window.cpp" line="87"/>
+        <location filename="../source/eigenbrot_window.cpp" line="88"/>
         <source>Ctrl+R</source>
         <translation>Ctrl+R</translation>
     </message>
     <message>
-        <location filename="../source/eigenbrot_window.cpp" line="88"/>
+        <location filename="../source/eigenbrot_window.cpp" line="89"/>
         <source>Display real and imaginary components</source>
         <translation>Reelle und imaginäre Komponenten anzeigen</translation>
     </message>
     <message>
-        <location filename="../source/eigenbrot_window.cpp" line="90"/>
+        <location filename="../source/eigenbrot_window.cpp" line="91"/>
         <source>&amp;Magnitude/Phase</source>
         <translation>&amp;Absolutwert/Phase</translation>
     </message>
     <message>
-        <location filename="../source/eigenbrot_window.cpp" line="93"/>
+        <location filename="../source/eigenbrot_window.cpp" line="94"/>
         <source>Ctrl+M</source>
         <translation>Ctrl+A</translation>
     </message>
     <message>
-        <location filename="../source/eigenbrot_window.cpp" line="94"/>
+        <location filename="../source/eigenbrot_window.cpp" line="95"/>
         <source>Display magnitude and phase</source>
         <translation>Absolutwerte und Phasen anzeigen</translation>
     </message>
     <message>
-        <location filename="../source/eigenbrot_window.cpp" line="122"/>
+        <location filename="../source/eigenbrot_window.cpp" line="123"/>
         <source>&amp;File</source>
         <translation>&amp;Datei</translation>
     </message>
     <message>
-        <location filename="../source/eigenbrot_window.cpp" line="129"/>
+        <location filename="../source/eigenbrot_window.cpp" line="130"/>
         <source>&amp;Display</source>
         <translation>&amp;Anzeige</translation>
     </message>
     <message>
-        <location filename="../source/eigenbrot_window.cpp" line="130"/>
+        <location filename="../source/eigenbrot_window.cpp" line="131"/>
         <source>Components</source>
         <translation>Komponenten</translation>
     </message>
     <message>
-        <location filename="../source/eigenbrot_window.cpp" line="135"/>
+        <location filename="../source/eigenbrot_window.cpp" line="136"/>
         <source>&amp;Colours</source>
         <translation>&amp;Farben</translation>
     </message>
     <message>
-        <location filename="../source/eigenbrot_window.cpp" line="144"/>
+        <location filename="../source/eigenbrot_window.cpp" line="145"/>
         <source>File</source>
         <translation>Datei</translation>
     </message>
     <message>
-        <location filename="../source/eigenbrot_window.cpp" line="167"/>
+        <location filename="../source/eigenbrot_window.cpp" line="168"/>
         <source>Image Files (*.fits *.fit *.png *.tif *.jpg *.bmp *.gif);;All files (*.*)</source>
         <translation>Bilddateien (*.fits *.fit *.png *.tif *.jpg *.bmp *.gif);;Alle Dateien (*.*)</translation>
     </message>
     <message>
-        <location filename="../source/eigenbrot_window.cpp" line="174"/>
+        <location filename="../source/eigenbrot_window.cpp" line="172"/>
         <source>Read file</source>
         <translation>Datei lesen</translation>
+    </message>
+    <message>
+        <location filename="../source/eigenbrot_window.cpp" line="203"/>
+        <source>FITS Files (*.fits *.fit;;All files (*.*)</source>
+        <translation>FITS Dateien (*.fits *.fit;;All files (*.*)</translation>
+    </message>
+    <message>
+        <location filename="../source/eigenbrot_window.cpp" line="206"/>
+        <source>Save file</source>
+        <translation>Datei speichern</translation>
+    </message>
+</context>
+<context>
+    <name>FormulaDialog</name>
+    <message>
+        <location filename="../source/formula_dialog.cpp" line="91"/>
+        <source> (formula incomplete)</source>
+        <translation> (unvollständige Formel)</translation>
+    </message>
+    <message>
+        <location filename="../source/formula_dialog.cpp" line="94"/>
+        <source>&lt;/font&gt;&amp;nbsp;(parse failed at start of red text)</source>
+        <translation>&lt;/font&gt;&amp;nbsp;(parsen gescheitert am Anfang der roten Schrift)</translation>
     </message>
 </context>
 <context>
     <name>QObject</name>
     <message>
-        <location filename="../source/complex_array.cpp" line="85"/>
+        <location filename="../source/complex_array.cpp" line="86"/>
         <source>&quot;%1&quot; doesn&apos;t appear to exist!</source>
         <translation>&quot;%1&quot; existiert anscheinend nicht!</translation>
     </message>
     <message>
-        <location filename="../source/complex_array.cpp" line="91"/>
+        <location filename="../source/complex_array.cpp" line="92"/>
         <source>Couldn&apos;t open &quot;%1&quot; for reading</source>
         <translation>Konnte &quot;%1&quot; nicht lesen</translation>
     </message>
     <message>
-        <location filename="../source/complex_array.cpp" line="95"/>
+        <location filename="../source/complex_array.cpp" line="96"/>
         <source>Unable to determine file type of &quot;%1&quot;</source>
         <translation>Konnte Datei &quot;%1&quot; nicht entziffern</translation>
     </message>
     <message>
-        <location filename="../source/complex_array.cpp" line="124"/>
+        <location filename="../source/complex_array.cpp" line="125"/>
         <source>File &quot;%1&quot; load failed: unsupported format?</source>
         <translation>Lesen von &quot;%1&quot; fehlgeschlagen: Dateiformat nicht unterstützt?</translation>
     </message>
     <message>
-        <location filename="../source/complex_array.cpp" line="165"/>
+        <location filename="../source/complex_array.cpp" line="166"/>
         <source>Can&apos;t open &quot;%1&quot; for reading</source>
         <translation>Kann &quot;%1&quot; nicht lesen</translation>
     </message>
     <message>
-        <location filename="../source/complex_array.cpp" line="170"/>
+        <location filename="../source/complex_array.cpp" line="171"/>
         <source>&quot;%1&quot; has no NAXIS key</source>
         <translation>&quot;%1&quot; hat kein NAXIS Schlüssel</translation>
     </message>
     <message>
-        <location filename="../source/complex_array.cpp" line="175"/>
+        <location filename="../source/complex_array.cpp" line="176"/>
         <source>&quot;%1&quot; has no BITPIX key</source>
         <translation>&quot;%1&quot; hat kein BITPIX Schlüssel</translation>
     </message>
     <message>
-        <location filename="../source/complex_array.cpp" line="183"/>
+        <location filename="../source/complex_array.cpp" line="184"/>
         <source>Axis data missing for axis #%1 of %2</source>
         <translation>Fehlende Daten für Achse #%1 von %2</translation>
     </message>
     <message>
-        <location filename="../source/complex_array.cpp" line="199"/>
+        <location filename="../source/complex_array.cpp" line="200"/>
         <source>The data in &quot;%1&quot; are badly formatted</source>
         <translation>Die Datn in &quot;%1&quot; sind falsch formattiert</translation>
     </message>
@@ -235,99 +258,105 @@
     <name>formulaDialog</name>
     <message>
         <location filename="../ui/formula_dialog.ui" line="14"/>
-        <location filename="../../build/Debug/ui_formula_dialog.h" line="290"/>
+        <location filename="../../build/Debug/ui_formula_dialog.h" line="311"/>
         <source>Create image</source>
         <translation>Datensatz erzeugen</translation>
     </message>
     <message>
         <location filename="../ui/formula_dialog.ui" line="41"/>
-        <location filename="../../build/Debug/ui_formula_dialog.h" line="291"/>
+        <location filename="../../build/Debug/ui_formula_dialog.h" line="312"/>
         <source>For&amp;mula:</source>
         <translation>For&amp;mel:</translation>
     </message>
     <message>
-        <location filename="../ui/formula_dialog.ui" line="60"/>
-        <location filename="../../build/Debug/ui_formula_dialog.h" line="292"/>
+        <location filename="../ui/formula_dialog.ui" line="76"/>
+        <location filename="../../build/Debug/ui_formula_dialog.h" line="314"/>
         <source>Size</source>
         <translation>Bildgröße</translation>
     </message>
     <message>
-        <location filename="../ui/formula_dialog.ui" line="77"/>
-        <location filename="../../build/Debug/ui_formula_dialog.h" line="293"/>
+        <location filename="../ui/formula_dialog.ui" line="93"/>
+        <location filename="../../build/Debug/ui_formula_dialog.h" line="315"/>
         <source>&amp;Width</source>
         <translation>&amp;Breite</translation>
     </message>
     <message>
-        <location filename="../ui/formula_dialog.ui" line="87"/>
-        <location filename="../../build/Debug/ui_formula_dialog.h" line="294"/>
+        <location filename="../ui/formula_dialog.ui" line="103"/>
+        <location filename="../../build/Debug/ui_formula_dialog.h" line="316"/>
         <source>S&amp;quare</source>
         <translation>&amp;Quadradisch</translation>
     </message>
     <message>
-        <location filename="../ui/formula_dialog.ui" line="101"/>
-        <location filename="../../build/Debug/ui_formula_dialog.h" line="295"/>
+        <location filename="../ui/formula_dialog.ui" line="117"/>
+        <location filename="../../build/Debug/ui_formula_dialog.h" line="317"/>
         <source>&amp;Height</source>
         <translation>&amp;Höhe</translation>
     </message>
     <message>
-        <location filename="../ui/formula_dialog.ui" line="137"/>
-        <location filename="../../build/Debug/ui_formula_dialog.h" line="296"/>
+        <location filename="../ui/formula_dialog.ui" line="153"/>
+        <location filename="../../build/Debug/ui_formula_dialog.h" line="318"/>
         <source>Dimension</source>
         <translation>Dimensionen</translation>
     </message>
     <message>
-        <location filename="../ui/formula_dialog.ui" line="152"/>
-        <location filename="../../build/Debug/ui_formula_dialog.h" line="297"/>
+        <location filename="../ui/formula_dialog.ui" line="168"/>
+        <location filename="../../build/Debug/ui_formula_dialog.h" line="319"/>
         <source>&amp;1 dimension</source>
         <translation>&amp;1 Dimension</translation>
     </message>
     <message>
-        <location filename="../ui/formula_dialog.ui" line="159"/>
-        <location filename="../../build/Debug/ui_formula_dialog.h" line="298"/>
+        <location filename="../ui/formula_dialog.ui" line="175"/>
+        <location filename="../../build/Debug/ui_formula_dialog.h" line="320"/>
         <source>&amp;2 dimensions</source>
         <translation>&amp;2 Dimensionen</translation>
     </message>
     <message>
-        <location filename="../ui/formula_dialog.ui" line="183"/>
-        <location filename="../../build/Debug/ui_formula_dialog.h" line="299"/>
+        <location filename="../ui/formula_dialog.ui" line="199"/>
+        <location filename="../../build/Debug/ui_formula_dialog.h" line="321"/>
         <source>Multiple images</source>
         <translation>Mehrfachmodus</translation>
     </message>
     <message>
-        <location filename="../ui/formula_dialog.ui" line="200"/>
-        <location filename="../../build/Debug/ui_formula_dialog.h" line="300"/>
+        <location filename="../ui/formula_dialog.ui" line="216"/>
+        <location filename="../../build/Debug/ui_formula_dialog.h" line="322"/>
         <source>&amp;First value:</source>
         <translation>&amp;Anfangswert:</translation>
     </message>
     <message>
-        <location filename="../ui/formula_dialog.ui" line="224"/>
-        <location filename="../../build/Debug/ui_formula_dialog.h" line="301"/>
+        <location filename="../ui/formula_dialog.ui" line="240"/>
+        <location filename="../../build/Debug/ui_formula_dialog.h" line="323"/>
         <source>Fi&amp;nal value:</source>
         <translation>E&amp;ndwert:</translation>
     </message>
     <message>
-        <location filename="../ui/formula_dialog.ui" line="234"/>
-        <location filename="../../build/Debug/ui_formula_dialog.h" line="302"/>
+        <location filename="../ui/formula_dialog.ui" line="250"/>
+        <location filename="../../build/Debug/ui_formula_dialog.h" line="324"/>
         <source>In&amp;crement:</source>
         <translation>&amp;Zuwachs:</translation>
     </message>
     <message>
-        <location filename="../ui/formula_dialog.ui" line="251"/>
-        <location filename="../../build/Debug/ui_formula_dialog.h" line="303"/>
+        <location filename="../ui/formula_dialog.ui" line="267"/>
+        <location filename="../../build/Debug/ui_formula_dialog.h" line="325"/>
         <source>Mu&amp;ltiview window</source>
         <translation>Ge&amp;stapelte Bilder</translation>
     </message>
     <message>
-        <location filename="../ui/formula_dialog.ui" line="282"/>
-        <location filename="../../build/Debug/ui_formula_dialog.h" line="305"/>
-        <source>Cancel</source>
-        <translation>Abbrechen</translation>
+        <location filename="../ui/formula_dialog.ui" line="298"/>
+        <location filename="../../build/Debug/ui_formula_dialog.h" line="327"/>
+        <source>&amp;Help</source>
+        <translation>&amp;Hilfe</translation>
     </message>
     <message>
-        <location filename="../ui/formula_dialog.ui" line="289"/>
-        <location filename="../../build/Debug/ui_formula_dialog.h" line="306"/>
-        <source>OK</source>
-        <translation>OK</translation>
+        <location filename="../ui/formula_dialog.ui" line="312"/>
+        <location filename="../../build/Debug/ui_formula_dialog.h" line="329"/>
+        <source>C&amp;ancel</source>
+        <translation>A&amp;bbrechen</translation>
+    </message>
+    <message>
+        <location filename="../ui/formula_dialog.ui" line="319"/>
+        <location filename="../../build/Debug/ui_formula_dialog.h" line="330"/>
+        <source>&amp;OK</source>
+        <translation>&amp;OK</translation>
     </message>
 </context>
 </TS>
