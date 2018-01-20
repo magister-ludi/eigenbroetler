38d533a4b0724553e69cc169174968a3961a0459
Michael Oldfield <magister_ludi_github@yahoo.com.au>
2018-01-20 16:09:11 +1100

    Removed support for maintaining translations.

--- cpp/source/main.cpp
+++ cpp/source/main.cpp
@@ -2,17 +2,13 @@
 #include <QApplication>
 #include <QFile>
 #include <QTextCodec>
-#include <QTranslator>
 #include <eigenbroetler_window.h>
 
 int main(int argc, char *argv[])
 {
     Q_INIT_RESOURCE(eigenbrot);
     QApplication app(argc, argv);
-    QTranslator translator;
     QTextCodec::setCodecForTr(QTextCodec::codecForName("UTF-8"));
-    translator.load(QFile::decodeName(QString("eigenbroetler_de").toAscii()));
-    app.installTranslator(&translator);
 
     EigenbroetlerWindow *mainWin = EigenbroetlerWindow::instance();
 #if defined(Q_OS_SYMBIAN)
